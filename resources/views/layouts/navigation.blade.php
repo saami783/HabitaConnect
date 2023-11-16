b735127cf9bf8a2c428e4c1f2a1324c24aed61d8
Saami783 <sami.bahij1@gmail.com>
2023-11-16 15:32:58 +0100

    ajout d'une redirection accueil dans le profil

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,10 +2,11 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            @can('view_admin_dashboard')
+
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    @can('view_admin_dashboard')
                     <x-nav-link :href="route('admin')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
@@ -30,9 +31,16 @@
                     <x-nav-link :href="route('admin.reviews')" :active="request()->routeIs('dashboard')">
                         {{ __('Avis') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.reviews')" :active="request()->routeIs('dashboard')">
+                        {{ __('Avis') }}
+                    </x-nav-link>
+                    @endcan
+                        <x-nav-link :href="route('announces.index')" :active="request()->routeIs('dashboard')">
+                            {{ __('Accueil') }}
+                        </x-nav-link>
                 </div>
             </div>
-            @endcan
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">

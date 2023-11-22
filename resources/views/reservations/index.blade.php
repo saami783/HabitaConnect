1b3f1a61f5ae71e120092b4279171f9d8ac88cfa
Hossein <hzaer@yahoo.com>
2023-11-22 13:24:51 +0100

    v 0.8, no comment, no design for cancelation

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,9 +1,15 @@
 @extends('welcome')
-
+ <link rel="stylesheet" href="{{asset('css/reservation_style/reservation_CSS.css')}}">
 @section('content')
+
+    <div class="h1">
+        <h1><strong>Mes reservations</strong></h1>
+    </div>
+    <div class="main">
     @foreach($reservations as $reservation)
         <p>
             <a href="{{ route('reservations.show', $reservation) }}"> Reservation numéro {{ $reservation->id }}</a>
         </p>
     @endforeach
+    </div>
 @endsection

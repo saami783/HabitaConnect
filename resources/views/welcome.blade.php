67993c7064031e2d66e9313ec5d570760afa1b22
Hossein <hzaer@yahoo.com>
2023-11-22 13:24:51 +0100

    v 0.8, no comment, no design for cancelation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="{{ asset('css/style.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/pagination/pagination_CSS.css')}}" rel="stylesheet" type="text/css" />
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -76,8 +77,10 @@
                 </div>
             </header>
 
-            <main class="py-4">
-                @yield('content')
+            <main>
+                <section class="main_section">
+                    @yield('content')
+                </section>
             </main>
         </body>
 </html>

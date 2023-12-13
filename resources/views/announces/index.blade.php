f044c76ac8c3ed0c009abfb5a1794e5cb4fd7a46
Hossein <hzaer@yahoo.com>
2023-12-13 14:54:13 +0100

    sans affichage de l'image

--- resources/views/announces/index.blade.php
+++ resources/views/announces/index.blade.php
@@ -7,15 +7,18 @@
         <section class="annonce">
             <div class="div_offre">
                 @if($announce->files->isEmpty())
-                    <img src="{{asset('images/images/no_image_disponible.svg')}}" alt="sans image" title="Aucune image disponible pour cette offre" class="offre_image">
+                    <div class="div_offre_image">
+                        <img src="{{asset('images/images/no_image_disponible.svg')}}" alt="sans image" title="Aucune image disponible pour cette offre" class="offre_image">
+                    </div>
                 @else
-                        @foreach($announce->files as $file)
-                            <div class="div_offre_image">
-                                <img src="{{ asset($file->file_path) }}" alt="Announce Image" style="width:100px; height:auto;" class="offre_image">
-                            </div>
-                        @endforeach
+                    @foreach($announce->files as $file)
+                        <div class="div_offre_image">
+                            <img src="{{ asset($file->file_path) }}" alt="Announce Image" style="width:100px; height:auto;" class="offre_image">
+                        </div>
+                    @endforeach
                 @endif
             </div>
+
             <hr />
             <div class="offre_detail">
                 <p><a href="{{ route('announces.show', $announce) }}">{{ $announce->title }}</a></p>
--- resources/views/reservations/show.blade.php
+++ resources/views/reservations/show.blade.php
@@ -1,33 +1,37 @@
 @extends('welcome')
+<link href="{{ asset('css/detailOffre_CSS.css')}}" rel="stylesheet" type="text/css"/>
 
 @section('content')
-    <p> Id : {{ $reservation->id }}</p>
-    <p> Begin at :  {{ $reservation->begin_at }} </p>
-    <p> End at : {{ $reservation->end_at }} </p>
-    <p> User id : {{ $reservation->user_id }} </p>
-    <p> Announce id : {{ $reservation->announce_id }} </p>
-    <p> Price : {{ $reservation->price }} </p>
-    <p> Total days : {{ $reservation->total_days }} </p>
-    <p> Status : {{ $reservation->status }} </p>
+    <section id="reservation_item">
+        <p id="reservation_id"> Id : {{ $reservation->id }}</p>
+        <p id="reservation_date-debut"> Begin at : {{ $reservation->begin_at }} </p>
+        <p id="reservation_date-fin"> End at : {{ $reservation->end_at }} </p>
+        <p id="reservation_user-id"> User id : {{ $reservation->user_id }} </p>
+        <p id="reservation_annonce-id"> Announce id : {{ $reservation->announce_id }} </p>
+        <p id="reservation_prix-total"> Price : {{ $reservation->price }} </p>
+        <p id="reservation_jours"> Total days : {{ $reservation->total_days }} </p>
+        <p id="reservation_status"> Status : {{ $reservation->status }} </p>
 
-    @if($reservation->status === "Réservation non finalisée")
-        <form action="/session" method="POST">
-            <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <input type='hidden' name="total" value="{{ $reservation->price }}">
-            <input type='hidden' name="productname" value="{{ $reservation->announce_id }}">
-            <input type='hidden' name="reservation" value="{{ $reservation->id }}">
-            <button class="btn btn-success" type="submit" id="checkout-live-button">Payer</button>
-        </form>
-
-        <div class="card-footer text-muted">
-            {{-- Formulaire de suppression --}}
-            <form action="{{ route('reservations.destroy', $reservation) }}" method="POST" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cette reservation ?');">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Supprimer</button>
+        @if($reservation->status === "Réservation non finalisée")
+            <form action="/session" method="POST">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                <input type='hidden' name="total" value="{{ $reservation->price }}">
+                <input type='hidden' name="productname" value="{{ $reservation->announce_id }}">
+                <input type='hidden' name="reservation" value="{{ $reservation->id }}">
+                <button class="btn btn-success" type="submit" id="checkout-live-button">Payer</button>
             </form>
-        </div>
-    @else
-        <p> <a href="{{ route('generatePdf', $reservation) }}"> Ma facture </a> </p>
-    @endif
+
+            <div class="card-footer text-muted">
+                {{-- Formulaire de suppression --}}
+                <form action="{{ route('reservations.destroy', $reservation) }}" method="POST"
+                      onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cette reservation ?');">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Supprimer</button>
+                </form>
+            </div>
+        @else
+            <p ><a href="{{ route('generatePdf', $reservation) }}"> Ma facture </a></p>
+        @endif
+    </section>
 @endsection
